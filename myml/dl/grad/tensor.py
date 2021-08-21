ec92d83de415254bbe90854df2dd1eb2958524a5
NetherQuartz <vd-larkin@ya.ru>
2021-08-22 00:13:26 +0300

    Add differentiation with respect to one scalar variable

--- myml/dl/grad/tensor.py
+++ myml/dl/grad/tensor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 from typing import Union
+from functools import reduce
 
 from .graph import Node
 
@@ -11,12 +12,22 @@ class Tensor:
     def __init__(self,
                  array: Union[np.ndarray, list],
                  func: str = None,
-                 args: list = None,
-                 requires_grad: bool = False):
+                 args: list[Node] = None,
+                 requires_grad: bool = None,
+                 dtype=np.float32):
 
-        self._array: np.ndarray = np.array(array)
+        self._array: np.ndarray = np.array(array, dtype=dtype)
         self._graph: Node = Node(self, func, args)
-        self._requires_grad: bool = requires_grad
+
+        if requires_grad is not None:
+            self._requires_grad = requires_grad
+        elif len(self._graph.args) > 0:
+            self._requires_grad = reduce(lambda t, a: t.tensor.requires_grad or a.tensor.requires_grad,
+                                         self._graph.args)
+        else:
+            self._requires_grad = False
+
+        self._grad: Tensor = None
 
     @property
     def array(self):
@@ -34,15 +45,60 @@ class Tensor:
     def requires_grad(self, value):
         self._requires_grad = value
 
+    @property
+    def grad(self):
+        return self._grad
+
+    @property
+    def graph(self):
+        return self._graph
+
     def __add__(self, other: Tensor) -> Tensor:
         return Tensor(array=self._array + other._array,
-                      func="+", args=[self._graph, other._graph],
-                      requires_grad=(self._requires_grad or other._requires_grad))
+                      func="+", args=[self._graph, other._graph])
 
     def __mul__(self, other: Tensor) -> Tensor:
         return Tensor(array=self._array * other._array,
-                      func="*", args=[self._graph, other._graph],
-                      requires_grad=(self._requires_grad or other._requires_grad))
+                      func="*", args=[self._graph, other._graph])
+
+    def __pow__(self, other: Tensor) -> Tensor:
+        return Tensor(array=self._array ** other._array, func="**", args=[self._graph, other._graph])
 
     def __str__(self) -> str:
         return str(self._array)
+
+    @staticmethod
+    def _diff(node: Node) -> Tensor:
+        if not node.tensor.requires_grad:
+            return Tensor([0])
+
+        if len(node.args) == 2:
+            a, b = node.args
+
+            if node.func == "+":
+                if a.tensor.requires_grad and b.tensor.requires_grad:
+                    return Tensor._diff(a) + Tensor._diff(b)
+                elif not a.tensor.requires_grad and b.tensor.requires_grad:
+                    return Tensor._diff(b)
+                elif a.tensor.requires_grad and not b.tensor.requires_grad:
+                    return Tensor._diff(a)
+                else:
+                    return Tensor([0])
+            elif node.func == "*":
+                if a.tensor.requires_grad and b.tensor.requires_grad:
+                    return Tensor._diff(a) * b.tensor + Tensor._diff(b) * node.args[
+                        0].tensor
+                elif not a.tensor.requires_grad and b.tensor.requires_grad:
+                    return a.tensor * Tensor._diff(b)
+                elif a.tensor.requires_grad and not b.tensor.requires_grad:
+                    return Tensor._diff(a) * b.tensor
+                else:
+                    return Tensor([0])
+            elif node.func == "**":
+                if a.tensor.requires_grad and not b.tensor.requires_grad:
+                    return b.tensor * a.tensor ** (b.tensor + Tensor([-1]))
+        else:
+            return Tensor([1])
+
+    def backward(self):
+        self._grad = Tensor._diff(self._graph)
--- tests/myml/dl/grad/test_tensor.py
+++ tests/myml/dl/grad/test_tensor.py
@@ -25,6 +25,18 @@ def test_get_array():
         a.array = np.array([0, 1])
 
 
+def test_get_graph():
+    a = Tensor([[1, 2], [3, 4]])
+    a *= a
+    assert id(a.graph) == id(a._graph)
+    with pytest.raises(AttributeError):
+        a.graph = None
+
+
+def test_get_grad():
+    pass
+
+
 def test_get_shape():
     a = Tensor([[1, 2], [3, 4]])
     assert len(a.shape) == len(a._array.shape) and reduce(lambda t, s: t[0] == t[1], zip(a._array.shape, a.shape))
@@ -52,3 +64,17 @@ def test_requires_grad_on_results():
     a = Tensor([[1, 2], [3, 4]], requires_grad=True)
     b = Tensor([[0, -1], [-1, 0]], requires_grad=True)
     assert (a + b).requires_grad is True
+
+
+def test_diff():
+    x = Tensor([3], requires_grad=True)
+    a = Tensor([4])
+    b = Tensor([5])
+
+    y = a * x + b
+    y.backward()
+    assert (y.grad.array == a.array).all()
+
+    y = a * x ** Tensor([2]) + b * x + b  # y = ax^2 + bx + b
+    y.backward()  # y' = 2ax + b
+    assert (y.grad.array == np.array([29])).all()
